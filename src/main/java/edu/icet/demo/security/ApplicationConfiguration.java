c88c00a1d30e8e5d49300714b961e886f19afc8d
Prageeth Thilakarathna <ygprageethchamara2002@gmail.com>
2024-11-24 16:20:44 +0530

    create user details service bean object

--- src/main/java/edu/icet/demo/security/ApplicationConfiguration.java
+++ src/main/java/edu/icet/demo/security/ApplicationConfiguration.java
@@ -1,17 +1,27 @@
 package edu.icet.demo.security;
 
+import edu.icet.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Locale;
+
 @Configuration
+@RequiredArgsConstructor
 public class ApplicationConfiguration {
+
+    private final UserRepository userRepository;
+    private final MessageSource msgSrc;
 
     @Bean
     public RestTemplate restTemplate() {
@@ -32,5 +42,12 @@ public class ApplicationConfiguration {
         authenticationProvider.setUserDetailsService(userDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder);
         return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        msgSrc.getMessage("exception.username_not_found", null, Locale.ENGLISH)));
     }
 }
